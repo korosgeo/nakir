2d1463d8f9c48fe16584b5b21553edb1c3c46c13
Georgii Korostii <george.korosty@gmail.com>
2025-08-30 00:15:52 +0200

    added logic to post and retrieve images

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/pressly/goose/v3"
 
 	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 func setupDbConnection() *sql.DB {
 	db, err := sql.Open("postgres", "user=postgres password=qwerty sslmode=disable")
@@ -40,17 +45,64 @@ func pingHandler(c *gin.Context) {
 	})
 }
 
+func saveImageHandler(c *gin.Context) {
+	file, err := c.FormFile("image")
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "No image received"})
+		return
+	}
+
+	ext := filepath.Ext(file.Filename)
+	filename := uuid.New().String() + ext
+	path := "./images/" + filename
+
+	if err := c.SaveUploadedFile(file, path); err != nil {
+		c.AbortWithStatusJSON(http.StatusMethodNotAllowed, gin.H{"message": "Cannot save file to the file system"})
+		return
+	}
+
+	db.Exec("INSERT INTO image (path) VALUES ($1)", path)
+	c.JSON(http.StatusOK, gin.H{"message": "Image uploaded", "filename": filename})
+}
+
+func getAllImagesHandler(c *gin.Context) {
+	var images []string
+	rows, err := db.Query("SELECT path FROM image")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Error while retrieving images"})
+	}
+	defer rows.Close()
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Failed to get the images"})
+		return
+	}
+
+	for rows.Next() {
+		var image string
+		if err := rows.Scan(&image); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Error while retrieving an image"})
+		}
+		images = append(images, image)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"images": images})
+}
+
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/ping", pingHandler)
-	// r.POST("/photo")
+	r.POST("/image", saveImageHandler)
+	r.GET("/image/all", getAllImagesHandler)
 
 	return r
 }
 
 func main() {
-	db := setupDbConnection()
+	db = setupDbConnection()
 	defer db.Close()
 
 	runMigrations(db)
